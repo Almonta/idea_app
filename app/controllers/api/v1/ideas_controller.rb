274040c527163e6b64530a9e9b3b77843965861d
motohiro takagi <tkg.8.mthr@gmail.com>
2021-09-28 07:20:51 +0900

    ルーティング変更

---
+++ app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  module V1
+
+    class IdeasController < ApplicationController
+      # skip_before_action :verify_authenticity_token
+
+      def index
+        @ideas = Idea.all
+        render json: @ideas
+      end
+
+      def create
+        @idea = Idea.new(idea_params)
+        if @idea.save
+          render json: @idea, status: :created
+        else
+          render json: @idea.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def idea_params
+        params.permit(:body, :category_id)
+      end
+    end
+
+  end
+end
