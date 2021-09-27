0f9bd7da8d6a2d232f6dbe933fbd8f13c170e195
motohiro takagi <tkg.8.mthr@gmail.com>
2021-09-27 19:57:18 +0900

    ideasテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'ideas/index'
+  get 'ideas/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories, only: [:new, :create]
   resources :ideas
