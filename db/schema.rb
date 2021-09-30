24c9a4d9782bd43ec20883075f95388178cca4d6
motohiro takagi <tkg.8.mthr@gmail.com>
2021-09-30 14:23:46 +0900

    rubocop修正 #3

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_105518) do
-
+ActiveRecord::Schema.define(version: 20_210_927_105_518) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "ideas", force: :cascade do |t|
-    t.text "body", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_ideas_on_category_id"
+  create_table 'ideas', force: :cascade do |t|
+    t.text 'body', null: false
+    t.bigint 'category_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['category_id'], name: 'index_ideas_on_category_id'
   end
 
-  add_foreign_key "ideas", "categories"
+  add_foreign_key 'ideas', 'categories'
 end
